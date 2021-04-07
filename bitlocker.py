c906f76a9cfedd58525611740e7d5c73a2f247de
Noema <510230392@qq.com>
2021-04-07 16:37:54 +0800

    新增Bitlocker FVEK密钥
    新增Win10 18362 64bits Bitlocker FVEK密钥解析

--- bitlocker.py
+++ bitlocker.py
@@ -5,8 +5,11 @@ import volatility.win32.tasks as tasks
 from volatility.renderers import TreeGrid
 from volatility.renderers.basic import Address
 import volatility.poolscan as poolscan
-class KeyPoolScan(poolscan.SinglePoolScanner):
+import binascii
+
+class MyPoolScan(poolscan.SinglePoolScanner):
     """ Pool scanner """
+
 class Bitlocker(common.AbstractWindowsCommand):
     """Extract Bitlocker FVEK. Supports Windows 7 - 10."""
     def __init__(self, config, *args, **kwargs):
@@ -17,6 +20,10 @@ class Bitlocker(common.AbstractWindowsCommand):
         'Fvec256' : 1008,
         'Cngb128' : 632,
         'Cngb256' : 672,
+	'None128' : 1280,
+	'None256' : 1500,
+        #'Cngb128' : 2048,
+        #'Cngb256' : 10240,
         }
         BLMode = {
         '00' : 'AES 128-bit with Diffuser',
@@ -24,7 +31,8 @@ class Bitlocker(common.AbstractWindowsCommand):
         '02' : 'AES 128-bit',
         '03' : 'AES 256-bit',
         '10' : 'AES 128-bit (Win 8+)',
-        '20' : 'AES 256-bit (Win 8+)'
+        '20' : 'AES 256-bit (Win 8+)',
+        '40' : 'AES 256-bit (Win 10)'
        }
 
         length = 16
@@ -33,12 +41,11 @@ class Bitlocker(common.AbstractWindowsCommand):
         winver = (address_space.profile.metadata.get("major", 0), address_space.profile.metadata.get("minor", 0))
         arch = address_space.profile.metadata.get("memory_model",0)
 
-
+	# Win7
         if winver < (6,2):
+            tweak = "Not Applicable"
             poolsize = lambda x : x >= PoolSize['Fvec128'] and x <= PoolSize['Fvec256']
-
-
-            scanner = KeyPoolScan()
+            scanner = MyPoolScan()
             scanner.checks = [
                 ('PoolTagCheck', dict(tag = "FVEc")),
                 ('CheckPoolSize', dict(condition = poolsize)),
@@ -66,20 +73,20 @@ class Bitlocker(common.AbstractWindowsCommand):
                 else:
                     length = 16
                 fvek_raw = address_space.zread(offset+fvekOffsetRel,length)
-                tweak = []
                 if mode == '01' or mode == '00':
-                    for o, h ,c in utils.Hexdump(address_space.zread(offset+tweakOffsetRel,length)):
-                        tweak.append(h)
-                yield pool, BLMode[mode], tweak, fvek_raw
-        if winver >= (6,2):
+		    tweak_raw = address_space.zread(offset+tweakOffsetRel,length)
+                yield pool, BLMode[mode], tweak_raw, fvek_raw
+
+	# Win8+, part of Win10
+        if winver >= (6,2) and winver < (6,4):
             tweak = "Not Applicable"
             poolsize = lambda x : x >= PoolSize['Cngb128'] and x <= PoolSize['Cngb256']
-            scanner = KeyPoolScan()
+            scanner = MyPoolScan()
             scanner.checks = [
                 ('PoolTagCheck', dict(tag = "Cngb")),
                 ('CheckPoolSize', dict(condition = poolsize)),
                 ('CheckPoolType', dict(paged = False, non_paged = True)),
-                     ]
+		]
 
 		# Quick, hacky fix as a temporary solution.
 		# https://pbs.twimg.com/media/Ce-B2sgXEAAqTQ_.jpg
@@ -103,10 +110,51 @@ class Bitlocker(common.AbstractWindowsCommand):
                     length = 32
                 else:
                     length = 16                 
-                f1 = address_space.zread(offset+fvek1OffsetRel,length)
-                f2 = address_space.zread(offset+fvek2OffsetRel,length)
-                if f1 == f2:
-                    yield pool, BLMode[mode], tweak, f2
+                fvek_raw = address_space.zread(offset+fvek1OffsetRel,length)
+                tweak_raw = address_space.zread(offset+fvek2OffsetRel,length)
+                if fvek_raw == tweak_raw and fvek_raw != binascii.a2b_hex('0' * (length * 2)):
+                    yield pool, BLMode[mode], tweak_raw, fvek_raw
+	
+	# >= Win10 18362
+	if winver >= (6,4):
+	    tweak = "Not Applicable"
+            poolsize = lambda x : x >= PoolSize['None128'] and x <= PoolSize['None256']
+            scanner = MyPoolScan()
+            scanner.checks = [
+                ('PoolTagCheck', dict(tag = "None")),
+                ('CheckPoolSize', dict(condition = poolsize)),
+                ('CheckPoolType', dict(paged = False, non_paged = True)),
+		]
+
+	    # 32bit system not supported 
+            #if (arch == '32bit'):
+            #    modeOffsetRel = 0x5C
+            #    fvek1OffsetRel = 0x60
+            #    fvek2OffsetRel = 0x84
+
+            if (arch == '64bit'):
+                modeOffsetRel = 0x98
+                fvek1OffsetRel = 0x9C
+                fvek2OffsetRel = 0xAC
+
+            for offset in scanner.scan(address_space):
+                pool = obj.Object("_POOL_HEADER", offset = offset, vm = address_space)
+                mode = address_space.zread(offset+modeOffsetRel,1)
+                for o, h, c in utils.Hexdump(mode):
+                    mode =h
+
+                if mode == '10':
+                    length = 16
+                elif mode == '20':
+                    length = 16                 
+	    	elif mode == '40':
+		    length = 32
+		    fvek2OffsetRel = 0xBC
+                fvek_raw = address_space.zread(offset+fvek1OffsetRel,length)
+                tweak_raw = address_space.zread(offset+fvek2OffsetRel,length)
+                if fvek_raw != tweak_raw and fvek_raw != binascii.a2b_hex('0' * (length * 2)):
+                    yield pool, BLMode[mode], tweak_raw, fvek_raw
+
 
 
     def unified_output(self, data):
@@ -116,21 +164,27 @@ class Bitlocker(common.AbstractWindowsCommand):
                                          ("TWEAK Key", str)
                                          ], self.generator(data))
     def generator(self, data):
-        for (pool, BLMode, tweak, fvek_raw) in data:
+        for (pool, BLMode, tweak_raw, fvek_raw) in data:
             fvek = []
+	    tweak = []
             for o, h, c in utils.Hexdump(fvek_raw):
                 fvek.append(h)
-            yield(0, [Address(pool),BLMode, str(''.join(fvek).replace(" ","")),str(''.join(tweak).replace(" ","")),])
+	    for o, h, c in utils.Hexdump(tweak_raw):
+		tweak.append(h)
+            yield(0, [Address(pool),BLMode, str(''.join(fvek).replace(" ","")), str(''.join(tweak).replace(" ","")),])
     def render_text(self, outfd, data):
         self.table_header(outfd, [("Address", "#018x"),
                                   ("Cipher", "32"),
                                   ("FVEK", "64"),
                                   ("TWEAK Key", "64"),
                                  ])
-        for (pool, BLMode, tweak, fvek_raw) in data:
+        for (pool, BLMode, tweak_raw, fvek_raw) in data:
             fvek = []
+	    tweak = []
             for o, h, c in utils.Hexdump(fvek_raw):
                 fvek.append(h)
+            for o, h, c in utils.Hexdump(tweak_raw):
+                tweak.append(h)
             self.table_row(outfd,
                            pool,
                            BLMode,
